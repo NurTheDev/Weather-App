c616cd9ab41679205f38dc1f060d511e6038abb4
NurTheDev <nur756.islam@gmail.com>
2025-01-21 00:44:55 +0600

    added tailwind component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 const apiKey = "b1c3221fbd656ef07a847aa0dc6b5ff0";
 const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangladesh";
-const getWeather = async (city) => {
+const getWeather = async () => {
   const response = await fetch(apiUrl + `&appid=${apiKey}`);
   const data = await response.json();
+  const temp = document.querySelectorAll(".temp");
+  const temp_max = document.querySelectorAll(".temp_max");
+  const temp_min = document.querySelectorAll(".temp_min");
+  temp.forEach((e) => {
+    e.innerHTML = data.main.temp;
+  });
+  temp_max.forEach((e) => {
+    e.innerHTML = `H: ${data.main.temp_max}`;
+  });
+  temp_min.forEach((e) => {
+    e.innerHTML = `L: ${data.main.temp_min}`;
+  });
   console.log(data);
 };
 getWeather();
+const getWeatherInfo = async (city) => {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  const data = await response.json();
+};
